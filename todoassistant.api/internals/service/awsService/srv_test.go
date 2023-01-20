f0e9af80cb8cbfc06721a6bc8b6fcf5e187708eb
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2023-01-20 14:56:57 +0100

    Added test to aws service

--- todoassistant.api/internals/service/awsService/srv_test.go
+++ todoassistant.api/internals/service/awsService/srv_test.go
@@ -2,9 +2,7 @@ package awsService
 
 import (
 	"log"
-	"mime/multipart"
 	"os"
-	"path/filepath"
 	"testing"
 
 	awss3 "test-va/internals/amazon/awsS3"
@@ -12,7 +10,7 @@ import (
 
 func TestUploadImage(t *testing.T) {
 	// create an awsSrv struct with the mock s3session
-	s3session, err := awss3.NewAWSSession("AKIA5G6AVJRXO42IGWER", "9C4gv28jjxk2dBxkl49+FO/SUksaGEnpYRncn5iq", "")
+	s3session, err := awss3.NewAWSSession("", "", "")
 	if err != nil {
 		log.Println("Error Connecting to AWS S3: ", err)
 		t.Errorf("Unexpected error: %v", err)
@@ -20,7 +18,10 @@ func TestUploadImage(t *testing.T) {
 
 	a := NewAWSSrv(s3session)
 	// create a mock file to be uploaded
-	mockFile := CreateFormFile()
+	mockFile, err := os.Create("mock_file.jpeg")
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
 
 	// call the UploadImage function
 	err = a.UploadImage(mockFile, "test_image.jpg")
@@ -29,35 +30,35 @@ func TestUploadImage(t *testing.T) {
 	}
 }
 
-func CreateFormFile() *multipart.FileHeader {
-	// create a mock file
-	currentDir, _ := os.Getwd()
-	path := filepath.Join(currentDir, "mock_image.jpg")
-	mockFile, err := os.Open(path)
-	// fmt.Println(path)
-	if err != nil {
-		panic(err)
-	}
-	defer mockFile.Close()
+// func CreateFormFile() *multipart.FileHeader {
+// 	// create a mock file
+// 	currentDir, _ := os.Getwd()
+// 	path := filepath.Join(currentDir, "mock_image.jpg")
+// 	mockFile, err := os.Open(path)
+// 	// fmt.Println(path)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer mockFile.Close()
 
-	// create header
-	header := make(map[string][]string)
-	header["Content-Disposition"] = []string{`form-data; name="file"; filename="mock_image.jpg"`}
-	header["Content-Type"] = []string{"image/jpeg"}
+// 	// create header
+// 	header := make(map[string][]string)
+// 	header["Content-Disposition"] = []string{`form-data; name="file"; filename="mock_image.jpg"`}
+// 	header["Content-Type"] = []string{"image/jpeg"}
 
-	// create a form file header
-	fileHeader := &multipart.FileHeader{
-		Filename: path,
-		Header:   header,
-		Size:     1289231,
-	}
+// 	// create a form file header
+// 	fileHeader := &multipart.FileHeader{
+// 		Filename: path,
+// 		Header:   header,
+// 		Size:     1289231,
+// 	}
 
-	// create a form
-	// form := &multipart.Form{
-	// 	File: map[string][]*multipart.FileHeader{
-	// 		"file": {fileHeader},
-	// 	},
-	// }
+// 	// create a form
+// 	// form := &multipart.Form{
+// 	// 	File: map[string][]*multipart.FileHeader{
+// 	// 		"file": {fileHeader},
+// 	// 	},
+// 	// }
 
-	return fileHeader
-}
+// 	return fileHeader
+// }
